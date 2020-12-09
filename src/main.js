82b2a51e49a27fd4c9fce62e1e36826bb0be6749
Yaxiang <liuyaxiang123@icloud.com>
2020-12-10 00:33:20 +0800

    引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 // 加载全局样式表
 import './styles/index.css'
+// 导入Vant组件库及样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 全局注册Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
